07b4415d258e4ce37353dedd9bcf4e3c5eaf197a
shaunsephton <shaun@praekeltconsulting.com>
2011-02-02 11:22:27 +0200

    initial sloppy experimental work

--- order/__init__.py
+++ order/__init__.py
@@ -3,7 +3,11 @@ from django.db.models.signals import class_prepared
 
 import order.models
 
-def create_model(name, order_field_names, relation):
+from django.views.decorators.csrf import csrf_protect
+from django.utils.decorators import method_decorator
+csrf_protect_m = method_decorator(csrf_protect)
+
+def create_model(name, order_field_names, relation, relation_app_label, relation_model_name, relation_app_model_label):
     """
     Create model and register with admin.
     """
@@ -22,7 +26,7 @@ def create_model(name, order_field_names, relation):
 
     # Create various order fields.
     fields = {
-        'item': models.ForeignKey('tube.AdactusClip')
+        'item': models.ForeignKey(relation)
     }
     for field in order_field_names:
         fields[field] = models.IntegerField()
@@ -36,10 +40,35 @@ def create_model(name, order_field_names, relation):
 
     # Set the model as part of order.models.
     setattr(order.models, name, model)
-    
+            
     # Create an Admin class and register it with admin.
     class Admin(admin.ModelAdmin):
-        list_display = ['item',] + order_field_names
+        list_display = ['item_link',] + order_field_names
+
+        def get_model_perms(self, request):
+            """
+            Return empty perms dict thus hiding the model from admin index.
+            """
+            return {}
+    
+        @csrf_protect_m
+        def changelist_view(self, request, extra_context=None):
+            from django.core.urlresolvers import reverse
+            list_url = reverse('admin:%s_%s_changelist' % (relation_app_label, relation_model_name.lower()))
+            add_url = reverse('admin:%s_%s_add' % (relation_app_label, relation_model_name.lower()))
+
+            return super(Admin, self).changelist_view(request, extra_context={
+                'add_url': add_url,
+                'list_url': list_url,
+                'relation_opts': relation._meta,
+            })
+
+        def item_link(self, obj):
+            from django.core.urlresolvers import reverse
+            url = reverse('admin:%s_%s_change' % (relation_app_label, relation_model_name.lower()), args=(obj.id,))
+            
+            return '<a href="%s">%s</a>' % (url, obj.item.title)
+        item_link.allow_tags = True
     
     admin.site.register(model, Admin)
     
@@ -59,9 +88,21 @@ def create_order_models(sender, **kwargs):
         return 
 
     modules = settings.ORDERABLE_MODELS.keys()
-    # XXX: There has to be a better way to do this.
-    app_model = ('%s.%s' % (sender.__module__, sender._meta.object_name)).replace('.models', '')
-    if app_model in modules:
-        model = create_model('%sOrderItem' % app_model.split('.')[-1], order_field_names=['home', 'videos'], relation=app_model)
+
+    # Compute labels. XXX: There has to be a better way to do this.
+    app_label = sender.__module__.replace('.models', '')
+    model_name = sender._meta.object_name
+    app_model_label = '%s.%s' % (app_label, model_name)
+    if app_model_label in modules:
+        app_label = '.'.join(app_label.split('.')[1:])
+        app_model_label = '%s.%s' % (app_label, model_name)
+        model = create_model(
+            '%sOrderItem' % model_name, 
+            order_field_names=['home', 'videos'], 
+            relation=sender,
+            relation_app_label=app_label,
+            relation_model_name=model_name,
+            relation_app_model_label=app_model_label,
+        )
     
 class_prepared.connect(create_order_models)
--- order/admin.py
+++ order/admin.py
@@ -1,1 +1,11 @@
+from django.conf import settings
+from django.contrib import admin
 
+for model, model_admin in admin.site._registry.items():
+    
+    modules = settings.ORDERABLE_MODELS.keys()
+    
+    app_model = ('%s.%s' % (model.__module__, model._meta.object_name)).replace('.models', '')
+    
+    if app_model in modules:
+        model_admin.change_list_template = 'order/order_change_list.html'
