84357fff2b88bc23847b7ae6589326f192ee1402
shaunsephton <shaun@praekeltconsulting.com>
2011-02-28 13:43:12 +0200

    version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-order',
-    version='0.0.4',
+    version='0.0.5',
     description='Django app providing arbitrary, admin user specified, object ordering.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Praekelt Foundation',
