26d143a5dc0814af548fddf05bf8b93a1dac5aeb
shaunsephton <shaun@praekeltconsulting.com>
2011-02-28 13:46:00 +0200

    slight name refactor

--- order/admin.py
+++ order/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 
 from order.utils import is_orderable
 
-# Set change_list_template on orderable models' admin classes to user the order_change_list template, thus enabling 'Order' tool.
+# Set change_list_template on orderable models' admin classes to use the order_change_list template, thus enabling 'Order' tool.
 for model, model_admin in admin.site._registry.items():
-    if is_orderable(model):
+    label = '.'.join([model._meta.app_label, model._meta.object_name])
+    if is_orderable(label):
         model_admin.change_list_template = 'order/order_change_list.html'
