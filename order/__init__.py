0cd399234909934662aec29af34b9d10468a3bd8
shaunsephton <shaun@praekeltconsulting.com>
2011-02-28 13:46:22 +0200

    no longer use class prepared

--- order/__init__.py
+++ order/__init__.py
@@ -1,6 +1,5 @@
-from django.db.models.signals import class_prepared, post_save
+from django.db.models.signals import post_save
 
 from order import signal_handlers
 
-class_prepared.connect(signal_handlers.class_prepared)
 post_save.connect(signal_handlers.post_save)
