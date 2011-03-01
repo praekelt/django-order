efae4c6f4df27435c078b2105452f4966bdf5937
Alfred <alfred@praekelt.com>
2011-03-01 10:28:05 +0200

    added sort direction to the manager

--- order/managers.py
+++ order/managers.py
@@ -1,6 +1,6 @@
 import order
 
-def user_order_by(self, field):
+def user_order_by(self, field, direction="+"):
     """
     Queryset method ordering objects by user ordering field.
     """
@@ -8,14 +8,15 @@ def user_order_by(self, field):
     model_label = order.utils.resolve_labels('.'.join([self.model._meta.app_label, self.model._meta.object_name]))
     orderitem_set = getattr(self.model, order.utils.resolve_order_item_related_set_name(model_label))
     order_model = orderitem_set.related.model
-    
+
     # Resolve ordering model table name.
     db_table = order_model._meta.db_table
-    
+
     # Add ordering field as extra queryset fields.
     pk_name = self.model._meta.pk.attname
+    import ipdb;ipdb.set_trace()
     extra_select = {
         field: '(SELECT %s from %s WHERE item_id=%s.%s)' % (field, db_table, self.model._meta.db_table, pk_name)
     }
-    return self.extra(select=extra_select).order_by(field)
+    return self.extra(select=extra_select).order_by("%s%s.%s" % (direction,db_table,field) )
 
