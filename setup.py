bac6d331481bcc198fe656b8f0345611d1f8c2ac
Shaun Sephton <shaunsephton@gmail.com>
2011-09-08 10:55:42 +0200

    use latest setuptest

--- setup.py
+++ setup.py
@@ -1,11 +1,4 @@
 from setuptools import setup, find_packages
-from setuptools.command.test import test
-
-def run_tests(self):
-    from setuptest.runtests import runtests
-    return runtests(self)
-test.run_tests = run_tests
-
 
 setup(
     name='django-order',
@@ -18,9 +11,9 @@ setup(
     url='http://github.com/praekelt/django-order',
     packages=find_packages(),
     include_package_data=True,
-    test_suite="order.tests",
+    test_suite="setuptest.SetupTestSuite",
     tests_require=[
-        'django-setuptest',
+        'django-setuptest>=0.0.6',
     ],
     classifiers = [
         "Programming Language :: Python",
