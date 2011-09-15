8548d42a12bc3e5b1eb0c5ad139b54f18cf5dcd1
Shaun Sephton <shaunsephton@gmail.com>
2011-09-15 11:01:13 +0200

    connect post syncdb signal

--- order/__init__.py
+++ order/__init__.py
@@ -1,5 +1,6 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_syncdb
 
-from order import signal_handlers
+from order import models, signal_handlers
 
 post_save.connect(signal_handlers.post_save)
+post_syncdb.connect(signal_handlers.post_syncdb, sender=models)
