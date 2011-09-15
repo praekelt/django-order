e5e0cd9fad9a76bcfc08c57029ca2eabd238a14b
Shaun Sephton <shaunsephton@gmail.com>
2011-09-15 11:00:53 +0200

    create order objects for obejcts after syncdb

--- order/signal_handlers.py
+++ order/signal_handlers.py
@@ -1,4 +1,4 @@
-from order.utils import is_orderable, resolve_labels, \
+from order.utils import create_order_objects, is_orderable, resolve_labels, \
         resolve_order_item_related_set_name, sanitize_order
 
 
@@ -23,3 +23,11 @@ def post_save(sender, instance, created, **kwargs):
                 fields[order_field_name] = 1
             orderitem_set.model.objects.create(item=instance, **fields)
             sanitize_order(orderitem_set.model)
+
+
+def post_syncdb(sender, created_models, **kwargs):
+    for model in created_models:
+        label = '.'.join([model._meta.app_label, model._meta.object_name])
+        order_fields = is_orderable(label)
+        if order_fields:
+            create_order_objects(model, order_fields)
